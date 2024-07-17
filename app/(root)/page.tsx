87f496b884b9067f241c0e37883cfa643e042f81
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-07-17 20:32:57 +0530

    commit - 2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import ToatlBalanceBox from '@/components/ToatlBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstname: 'MyName'}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"  
+            title="Welcome"
+            user={loggedIn?.firstname || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <ToatlBalanceBox 
+            accounts={ [] }
+            totalBanks={ 1 }
+            totalCurrentBalance={ 1250.35 }
+          
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
